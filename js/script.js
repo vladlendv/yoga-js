bf03c186b89fa3f58b9948d3360ee109502fa3cb
Vladlen Dvornikov <tubebiznes@gmail.com>
2021-03-09 17:45:40 +0500

    add ajax for form, and show form message

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 // Timer
 
-	let deadline = '2021-03-08';
+	let deadline = '2021-03-25';
 
 	function getTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -70,8 +70,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
 		updateClock();
 		let timeInterval = setInterval(updateClock, 1000);
-
-
 	};
 
 	setClock('timer', deadline);
@@ -101,7 +99,73 @@ window.addEventListener('DOMContentLoaded', function() {
 		document.body.style.overflow = '';
 	});
 
-	
+	//  Form
 
+	let message = new Object();
+	message.loading = 'Загрузка...';
+	message.success = 'Спасибо, скоро мы с вами свяжемся';
+	message.fail = 'Что-то пошло не так';
 
+	let form = document.getElementsByClassName('main-form')[0],
+		contactForm = document.getElementById('form'),
+		input = form.getElementsByTagName('input'),
+		statusMessage = document.createElement('div');
+	statusMessage.classList.add('status');
+
+	contactForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		contactForm.appendChild(statusMessage);
+		statusMessage.style.color = '#fff';
+		// AJAX
+		let request = new XMLHttpRequest();
+		request.open('POST', '../server.php');
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		let formData = new FormData(form);
+		request.send(formData);
+
+		request.onreadystatechange = function() {
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			} else if (request.readyState === 4) {
+				if (request.status == 200 && request.status < 300) {
+					statusMessage.innerHTML = message.success;
+					// add content
+				} else {
+					statusMessage.innerHTML = message.fail;
+				}
+			}
+		}
+		for (let i = 0; i < input.length; i++) {
+			// clear input area 
+			input[i].value = '';
+		}
+	});
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		form.appendChild(statusMessage);
+		// AJAX
+		let request = new XMLHttpRequest();
+		request.open('POST', '../server.php');
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		let formData = new FormData(form);
+		request.send(formData);
+
+		request.onreadystatechange = function() {
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			} else if (request.readyState === 4) {
+				if (request.status == 200 && request.status < 300) {
+					statusMessage.innerHTML = message.success;
+					// add content
+				} else {
+					statusMessage.innerHTML = message.fail;
+				}
+			}
+		}
+		for (let i = 0; i < input.length; i++) {
+			// clear input area 
+			input[i].value = '';
+		}
+	});
 });
